dc39145150b56ebcf372a6a94a3de99c5e6c46b2
imran <imranb2k@hotmail.com>
2019-03-01 12:01:02 +0000

    Star object model created

---
+++ src/main/java/com/ibapp/domain/model/Star.java
@@ -0,0 +1,23 @@
+package com.ibapp.domain.model;
+
+public class Star {
+
+    public Star() {
+    }
+
+    public Star(Long id) {
+        this.id = id;
+    }
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+}
